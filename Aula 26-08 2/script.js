cc96887d8657f832458c6afec0e5b4cb5cccdd17
MarconiCop <marconifrancisco@live.com>
2021-08-26 20:23:17 -0300

    Aula de 26/08 adicionada

---
+++ Aula 26-08 2/script.js
@@ -0,0 +1,15 @@
+var a=3;
+var b=4;
+var soma=a+b;
+
+/*
+if(soma >=5){
+    alert(soma);
+}*/
+
+var texto = "Isso é um texto em JavaScript!"
+//alert(texto);
+
+alert("Bem vindo " + texto + "!!!");
+
+document.write("<h1>Escrevendo com document.write!<h1>");
